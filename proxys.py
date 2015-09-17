30b10fac4d0f79d13a4bf284583e0b11f74e301c
jmpews <jmpews@gmail.com>
2015-09-18 00:46:09 +0800

    基本的框架

--- proxys.py
+++ proxys.py
@@ -1,136 +1,106 @@
 __author__ = 'jmpews'
 import socket
 from redisq import RedisQueue
-# sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# sock.connect(('58.220.2.133', 80))
-# sock.send("CONNECT github.com:443 HTTP/1.1\r\nHost: github.com:443\r\nProxy-Connection: keep-alive\r\n\r\n".encode())
-# while True:
-#     r=sock.recv(1024).decode()
-#     if r!='':
-#         print(r)
-
-# sock.connect(('206.41.89.72', 3389))
-# sock.send(b'\x05\x02\x00\x02')
-# while True:
-#     r=sock.recv(1024)
-#     if r!=b'':
-#         print(r)
-
-# 采用生成器方式,防止超长list爆内存
-def genips(s,e):
-    def s2n(str):
-        i=[int(x) for x in str.split('.')]
-        return i[0]<<24|i[1]<<16|i[2]<<8|i[3]
-    def n2ip(num):
-        return '%s.%s.%s.%s' % (
-            (num&0xFF000000)>>24,
-            (num&0x00FF0000)>>16,
-            (num&0x0000FF00)>>8,
-            (num&0x000000FF)
-        )
-    for t in range(s2n(s),s2n(e)):
-        yield n2ip(t)
-
-
-
-
-ips=genips('40.3.115.51','70.0.0.128')
-# ips=genips('110.0.0.1','126.0.0.1s28')
-# ips=genips('210.0.0.1','230.0.0.128')
-
-
-# for i in ips:
-#     pass
-# print(len(ips))
 import errno
-
-httports=[80,3128,8080,8888]
-
-inputs=[]
-outputs=[]
-
-def addips(ip):
-    socks=[]
-    for port in httports:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.setblocking(0)
-        err=sock.connect_ex((ip, port))
-        socks.append(sock)
-    return socks
-
-
 import select
 import time
-def sendhttp(x):
-    t=x.getpeername()
-    connstr="CONNECT %s:%s HTTP/1.1\r\nHost: %s:%s\r\nProxy-Connection: keep-alive\r\n\r\n" % (t[0],t[1],t[0],t[1])
-    x.send(connstr.encode())
+import utils
 
-def checkhttp(data):
-    if data.find(b'Connection established')==-1:
-        return False
-    return True
+# 采用非阻塞的connect,每个IP测试4个端口,手动做好每个socket的connect的超时处理
+# 几个注意点:
+# 1.IP的区段 http://ips.chacuo.net/
+# 2.非阻塞connect的返回码'EINPROGRESS',表示正在连接
+# 3.http代理的验证方式,send一段http报文,验证返回.
+
 
 rq=RedisQueue('proxy')
 
-while True:
-    # if len(outputs)<400:
-    #     for i in range(100-int(len(outputs)/4)):
-    #         try:
-    #             ip=ips.__next__()
-    #         except StopIteration:
-    #             # 循环到ip列表最后
-    #             break
-    #
-    #         outputs+=addips(ip)
-    outputs+=addips('40.3.219.211')
-    flag=0
-    readable,writeable,exceptional=select.select(inputs,outputs,[],3)
+ipfile=open('ip_shanghai.txt','r',encoding='utf-8')
+iplist=[]
+for line in ipfile:
+    tmp=line.split('\t')
+    iplist.append((tmp[0],tmp[1]))
 
+ips=utils.genips(iplist)
+inputs=[]
+outputs=[]
+outputimeouts=[]
+
+#test
+# outputimeouts+=utils.addips('118.144.108.254')
+
+while True:
+
+    # 维持select内outputs
+    if len(outputs)<400:
+        for i in range(100-int(len(outputs)/4)):
+            try:
+                ip=ips.__next__()
+            except StopIteration:
+                # 循环到ip列表最后
+                break
+            outputimeouts+=utils.addips(ip)
+
+
+    # 清除超时connect
+    # 由于非阻塞的connect,所以要手动排除超时的connect
+    outputimeouts=list(filter(utils.checktimeout,outputimeouts))
+    outputs=[x[0] for x in outputimeouts]
+    readable,writeable,exceptional=select.select(inputs,outputs,[],4)
+    print('Len-Write: ',len(writeable))
     for x in readable:
-        flag=1
         try:
             data=x.recv(1024)
+            print(data)
         except Exception as e:
             x.close()
             print(e)
-        if checkhttp(data):
+        if utils.checkhttp(data):
             detial=x.getpeername()
             print(detial)
             rq.put(detial[0]+':'+str(detial[1]))
-        else:
-            print(data)
         inputs.remove(x)
         x.close()
 
     for x in writeable:
-        print(x)
-        flag=1
         erro=x.getsockopt(socket.SOL_SOCKET, socket.SO_ERROR)
+        print(erro)
         # connect拒绝
         if erro==errno.ECONNREFUSED:
-            print('conn refused.')
-            outputs.remove(x)
+            print('conn refuse.')
+            outputimeouts=list(filter(lambda tm:tm[1]!=x.fileno(),outputimeouts))
+            # outputs.remove(x)
             x.close()
             continue
-        # 未建立连接
-        elif erro==errno.EINPROGRESS:
+
+        # 超时
+        elif erro==errno.ETIMEDOUT:
+            print('conn timeout.')
+            outputimeouts=list(filter(lambda tm:tm[1]!=x.fileno(),outputimeouts))
+            # outputs.remove(x)
+            x.close()
             continue
-        elif erro==errno.errno.EHOSTUNREACH:
+
+        # 不可到达
+        elif erro==errno.EHOSTUNREACH:
             print('host unreach.')
-            outputs.remove(x)
+            outputimeouts=list(filter(lambda tm:tm[1]!=x.fileno(),outputimeouts))
+            # outputs.remove(x)
             x.close()
             continue
+
+        # 正常connect
         # 发送http代理验证数据
-        sendhttp(x)
-        outputs.remove(x)
-        inputs.append(x)
+        elif erro==0:
+            print('connect success')
+            utils.sendhttp(x)
+            outputimeouts=list(filter(lambda tm:tm[1]!=x.fileno(),outputimeouts))
+            # outputs.remove(x)
+            inputs.append(x)
 
     for x in exceptional:
-        flag=1
         print('====EXCEP====')
+
     ip=ips.__next__()
     print(ip)
     print('loop...')
-    if not flag:
-        outputs=[]
---
+++ utils.py
@@ -0,0 +1,66 @@
+__author__ = 'jmpews'
+import socket
+import time
+
+#发送验证字符串
+def sendhttp(x):
+    t=x.getpeername()
+    connstr="CONNECT %s:%s HTTP/1.1\r\nHost: %s:%s\r\nProxy-Connection: keep-alive\r\n\r\n" % (t[0],t[1],t[0],t[1])
+    x.send(connstr.encode())
+
+#检查response是否存在字符串
+def checkhttp(data):
+    if data.find(b'Connection established')==-1:
+        return False
+    return True
+
+# 发送socks验证数据
+def sendsocks(x):
+    x.send(b'\x05\x02\x00\x02')
+
+#检查response是否存在字符串.
+def checksocks(data):
+    if data.find(b'\x05\x00') == -1:
+        return False
+    return True
+
+# 采用生成器方式,防止超长list爆内存
+def genips(ipl):
+    def s2n(str):
+        i=[int(x) for x in str.split('.')]
+        return i[0]<<24|i[1]<<16|i[2]<<8|i[3]
+    def n2ip(num):
+        return '%s.%s.%s.%s' % (
+            (num&0xFF000000)>>24,
+            (num&0x00FF0000)>>16,
+            (num&0x0000FF00)>>8,
+            (num&0x000000FF)
+        )
+    for s,e in ipl:
+        for t in range(s2n(s),s2n(e)):
+            yield n2ip(t)
+
+
+# 对于每个IP生成4个socket,表示检查4个常见端口
+def addips(ip):
+    httports=[80,3128,8080,8888]
+    socks=[]
+    tm=int(time.time())
+    for port in httports:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # 非阻塞connect
+        sock.setblocking(0)
+        err=sock.connect_ex((ip, port))
+        socks.append((sock,sock.fileno(),tm))
+    return socks
+
+# 检查sock是否超时
+def checktimeout(x):
+    t=time.time()
+    if x[2]+3<t:
+        try:
+            x.getpeername()
+            print('Exp:Host.')
+        except:
+            return False
+    return True
