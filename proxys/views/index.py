f8405743275c1be80ed0ec0806e6f5cf4ecb47b2
jmpews <jmpews@gmail.com>
2015-10-15 21:11:59 +0800

    使用flask做前端网站

--- proxys/views/index.py
+++ proxys/views/index.py
@@ -1,6 +1,6 @@
 __author__ = 'jmpews'
 
-from flask import Module
+from flask import Module,request
 from proxys.models import Proxy
 from flask import render_template
 
@@ -8,5 +8,7 @@ index_module=Module(__name__)
 
 @index_module.route('/')
 def index():
-    proxylist=Proxy.query.all()
+    r_type=request.args.get('proxytype','http')
+    # proxylist=Proxy.query.all()
+    proxylist=Proxy.query.filter_by(type=r_type).limit(1000).all()
     return render_template('index.html',proxylist=proxylist)
